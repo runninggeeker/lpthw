894da5fe9116164f5ecc1dbebe164d42277a804a
wuss <mrwushenshen@163.com>
2019-06-24 14:45:03 +0800

    wrote a ex01.py file

---
+++ ex01.py
@@ -0,0 +1,11 @@
+# /usr/bin/python
+# -*- coding: utf-8 -*-
+
+# 单行注释，对程序解释说明
+print('hello world')
+print('hello agin')
+print('I like typing this .')
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather than 'not'.")
+print('I "said" do not touch this.')
