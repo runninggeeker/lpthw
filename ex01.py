d1db25ba1cccf76c2a41d1b9b325f63b8ce784c0
wuss <mrwushenshen@163.com>
2019-06-24 14:52:00 +0800

    append GPL

--- ex01.py
+++ ex01.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 # 单行注释，对程序解释说明
-print('hello world')
-print('hello agin')
-print('I like typing this .')
+print('Hello World!')
+print('Hello agin!')
+print('I like typing this.')
 print("This is fun.")
 print("Yay! Printing.")
 print("I'd much rather than 'not'.")
